3170855c575badc8720a4e56076e60a364f00bbf
dede999 <andreluizsa00@gmail.com>
2018-06-02 02:24:01 -0300

    Atributo mulitvalorado 'habilidade' inserido juntamente à versão de esboço da dacumentação

--- scripts/DDL.sql
+++ scripts/DDL.sql
@@ -1,44 +1,58 @@
-CREATE TABLE IF NOT EXISTS membro (
-  mCPF            INT         NOT NULL,
+CREATE SCHEMA IF NOT EXISTS nacao_real;
+
+CREATE TABLE IF NOT EXISTS nacao_real.membro (
+  mCPF            BIGINT      NOT NULL,
   mID             SERIAL      NOT NULL  UNIQUE,
+  mNome           VARCHAR(30) NOT NULL,
+  mSnome          VARCHAR(30) NOT NULL,
   mEndereco       VARCHAR(90) NOT NULL,
   mCodPostal      VARCHAR(45) NOT NULL,
   mCidade         VARCHAR(45) NOT NULL,
   mEstado         VARCHAR(2)  NOT NULL,
   mPais           VARCHAR(45) NOT NULL,
   mESpecialidade  VARCHAR(45) NOT NULL,
-  mNasc           TIMESTAMP NOT NULL,
-  PRIMARY KEY (mCPF)
+  mNasc           TIMESTAMP   NOT NULL,
+  mPassword       VARCHAR(90) NOT NULL,
+  PRIMARY KEY (mID)
 );
 
-CREATE TABLE IF NOT EXISTS telefones (
+CREATE TABLE IF NOT EXISTS nacao_real.habilidades (
+  membro  INT NOT NULL,
+  habilidade VARCHAR(45),
+  PRIMARY KEY (membro, habilidade),
+  CONSTRAINT membro
+    FOREIGN KEY (membro)
+    REFERENCES nacao_real.membro (mID)
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS nacao_real.telefones (
   CodInternacional  INT         NOT NULL DEFAULT 55,
   Ntelefone         VARCHAR(20) NOT NULL,
-  TitularCPF        INT         NOT NULL,
+  TitularID         INT         NOT NULL,
   TipoContato       VARCHAR(45) NOT NULL,
   WhatsApp          BOOLEAN     NOT NULL DEFAULT TRUE,
-  PRIMARY KEY (TitularCPF, CodInternacional, Ntelefone),
-  CONSTRAINT TituarCPF
-    FOREIGN KEY (TitularCPF)
-    REFERENCES membro (mCPF)
+  PRIMARY KEY (TitularID, CodInternacional, Ntelefone),
+  CONSTRAINT TituarID
+    FOREIGN KEY (TitularID)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-CREATE INDEX TituarCPF_idx ON telefones (TitularCPF ASC);
 
-CREATE TABLE IF NOT EXISTS email (
+CREATE TABLE IF NOT EXISTS nacao_real.email (
   Email       INT NOT NULL,
-  TitularCPF  INT NOT NULL,
-  PRIMARY KEY (Email, TitularCPF),
+  TitularID  INT NOT NULL,
+  PRIMARY KEY (Email, TitularID),
   CONSTRAINT TitulaCPF
-    FOREIGN KEY (TitularCPF)
-    REFERENCES membro (mCPF)
+    FOREIGN KEY (TitularID)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
-CREATE INDEX TitulaCPF_idx ON email (TitularCPF ASC);
 
-CREATE TABLE IF NOT EXISTS celula (
+CREATE TABLE IF NOT EXISTS nacao_real.celula (
   idcelula    SERIAL  NOT NULL,
   celula_tipo INT     NOT NULL,
   -- pode ser internacional, nacional, estadual, municipal ou de bairro (combinar código)
@@ -48,56 +62,56 @@ CREATE TABLE IF NOT EXISTS celula (
   PRIMARY KEY (idcelula),
   CONSTRAINT celula_pai
     FOREIGN KEY (celula_pai)
-    REFERENCES celula (idcelula)
+    REFERENCES nacao_real.celula (idcelula)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
 
-CREATE TABLE IF NOT EXISTS inscricao (
+CREATE TABLE IF NOT EXISTS nacao_real.inscricao (
   participante  INT NOT NULL,
   celula        INT NOT NULL,
   PRIMARY KEY (participante, celula),
   CONSTRAINT participante
     FOREIGN KEY (participante)
-    REFERENCES membro (mCPF)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT celula
     FOREIGN KEY (celula)
-    REFERENCES celula (idcelula)
+    REFERENCES nacao_real.celula (idcelula)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS operacoes (
+CREATE TABLE IF NOT EXISTS nacao_real.operacoes (
   idoperacoes SERIAL      NOT NULL,
   operacao    VARCHAR(80) NOT NULL,
   PRIMARY KEY (idoperacoes)
 );
 
-CREATE TABLE IF NOT EXISTS credenciais (
+CREATE TABLE IF NOT EXISTS nacao_real.credenciais (
   membro    INT NOT NULL,
   celula    INT NOT NULL,
   operacao  INT NOT NULL,
   PRIMARY KEY (membro, celula, operacao),
   CONSTRAINT membro
     FOREIGN KEY (membro)
-    REFERENCES membro (mCPF)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT celula
     FOREIGN KEY (celula)
-    REFERENCES celula (idcelula)
+    REFERENCES nacao_real.celula (idcelula)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT operacao
     FOREIGN KEY (operacao)
-    REFERENCES operacoes (idoperacoes)
+    REFERENCES nacao_real.operacoes (idoperacoes)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS atividades (
+CREATE TABLE IF NOT EXISTS nacao_real.atividades (
   atividadeID     SERIAL  NOT NULL,
   atv_titulo      VARCHAR(90) NOT NULL,
   atv_local       VARCHAR(90) NULL,
@@ -106,39 +120,39 @@ CREATE TABLE IF NOT EXISTS atividades (
   PRIMARY KEY (atividadeID)
 );
 
-CREATE TABLE IF NOT EXISTS responsaveis (
+CREATE TABLE IF NOT EXISTS nacao_real.responsaveis (
   responsavel   INT NOT NULL,
   atividades    INT NOT NULL,
   PRIMARY KEY (responsavel, atividades),
   CONSTRAINT responsavel
     FOREIGN KEY (responsavel)
-    REFERENCES membro (mCPF)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT atividades
     FOREIGN KEY (atividades)
-    REFERENCES atividades (atividadeID)
+    REFERENCES nacao_real.atividades (atividadeID)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS super_credenciais (
+CREATE TABLE IF NOT EXISTS nacao_real.super_credenciais (
   membro INT NOT NULL,
   operacao INT NOT NULL,
   PRIMARY KEY (membro, operacao),
   CONSTRAINT membro
     FOREIGN KEY (membro)
-    REFERENCES membro (mCPF)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE CASCADE
     ON UPDATE CASCADE,
   CONSTRAINT operacao
     FOREIGN KEY (operacao)
-    REFERENCES operacoes (idoperacoes)
+    REFERENCES nacao_real.operacoes (idoperacoes)
     ON DELETE CASCADE
     ON UPDATE CASCADE
 );
 
-CREATE TABLE IF NOT EXISTS message (
+CREATE TABLE IF NOT EXISTS nacao_real.message (
   id_message  SERIAL NOT NULL,
   autor       INT NOT NULL,
   celula      INT NULL,
@@ -146,12 +160,12 @@ CREATE TABLE IF NOT EXISTS message (
   PRIMARY KEY (id_message),
   CONSTRAINT autor
     FOREIGN KEY (autor)
-    REFERENCES membro (mCPF)
+    REFERENCES nacao_real.membro (mID)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
   CONSTRAINT celula
     FOREIGN KEY (celula)
-    REFERENCES celula (idcelula)
+    REFERENCES nacao_real.celula (idcelula)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION
 );
